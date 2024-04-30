38461b59b7e416d4d9ce0ed7e2baf2d78bf17042
Jonathan Saina <sainajonathan@gmail.com>
2024-04-30 11:12:07 -0700

    2nd commit

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -9,35 +9,36 @@
  */
 void counting_sort(int *array, size_t size)
 {
-    int max = array[0];
-    int *count;
-    size_t i, j = 0;
+	int max = array[0];
+	int *count;
+	size_t i, j = 0;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+	return;
 
-    for (i = 1; i < size; i++) {
-        if (array[i] > max)
-            max = array[i];
-    }
+	for (i = 1; i < size; i++)
+	{
+	if (array[i] > max)
+	max = array[i];
+	}
 
-    count = calloc(max + 1, sizeof(int));
-    if (count == NULL)
-        return;
+	count = calloc(max + 1, sizeof(int));
+	if (count == NULL)
+	return;
 
-    for (i = 0; i < size; i++)
-        count[array[i]]++;
+	for (i = 0; i < size; i++)
+	count[array[i]]++;
 
-    printf("Counting array:");
-    for (i = 0; i <= (size_t)max; i++)
-        printf(" %d", count[i]);
-    printf("\n");
+	printf("Counting array:");
+	for (i = 0; i <= (size_t)max; i++)
+	printf(" %d", count[i]);
+	printf("\n");
 
-    for (i = 0; i <= (size_t)max; i++) 
-    {
-        while (count[i]-- > 0)
-            array[j++] = (int)i;
-    }
+	for (i = 0; i <= (size_t)max; i++)
+	{
+	while (count[i]-- > 0)
+	array[j++] = (int)i;
+	}
 
-    free(count);
+	free(count);
 }
